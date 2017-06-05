39e0ae63a8ebb2dc32321f6908114a9e540e5952
Michael Marango <mike.marango@gmail.com>
2017-06-05 09:35:23 -0700

    added web app

---
+++ MyIdeas/MyIdeas.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyIdeas.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MyIdeas/MyIdeas.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MyIdeas.Web.Startup))]
+
+namespace MyIdeas.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
